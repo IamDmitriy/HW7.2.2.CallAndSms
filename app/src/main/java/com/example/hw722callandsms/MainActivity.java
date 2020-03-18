11becfcf19ed689c9b7de1039a305dfb30b1ae3d
Dmitriy25 <ohapkin_da@mail.ru>
2020-03-18 16:11:22 +0300

    Реализовал вызов

---
+++ app/src/main/java/com/example/hw722callandsms/MainActivity.java
@@ -0,0 +1,82 @@
+package com.example.hw722callandsms;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int MY_PERMISSIONS_REQUEST_CALL_PHONE = 11;
+    private static final int MY_PERMISSIONS_REQUEST_SMS = 12;
+
+    private EditText edtPhone;
+    private EditText edtMessage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        init();
+    }
+
+    private void init() {
+        edtPhone = findViewById(R.id.edtPhone);
+        edtMessage = findViewById(R.id.edtMessage);
+        Button btnCall = findViewById(R.id.btnCall);
+        Button btnSms = findViewById(R.id.btnSms);
+
+        btnCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String number = edtPhone.getText().toString();
+
+                if (number.isEmpty()) {
+                    showToast("Пожалуйста, введите номер!");
+                    return;
+                }
+
+                call(number);
+
+            }
+        });
+    }
+
+    private void call(String number) {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) !=
+                PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.CALL_PHONE}, MY_PERMISSIONS_REQUEST_CALL_PHONE);
+        } else {
+            Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + number));
+            startActivity(intent);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_CALL_PHONE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    call(edtPhone.getText().toString());
+                } else {
+                    showToast("Резрешение на звонки отклонено");
+                }
+        }
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+}
